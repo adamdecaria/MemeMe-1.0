3594afc6020a70e7eb28eab38d1e7c85d3ac9a2a
Adam DeCaria <adamdecaria@gmail.com>
2016-10-08 18:51:08 -0400

    Initial Commit

---
+++ MemeTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MemeTest
+//
+//  Created by Adam DeCaria on 2016-10-08.
+//  Copyright © 2016 Adam DeCaria. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
